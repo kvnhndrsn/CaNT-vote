2dbcb14ea70aa6ccfdcc87bdb941e30ffbbc4c11
kevin henderson <umhendek@gmail.com>
2026-06-23 23:33:01 -0600

    update

---
+++ api/cron-activity.js
@@ -0,0 +1,10 @@
+import activityHandler from './activity-snapshot.js';
+
+export default function handler(req, res) {
+  if (req.headers['x-vercel-cron'] !== '1') {
+    return res.status(403).json({ error: 'forbidden' });
+  }
+  req.query = req.query || {};
+  req.query.key = process.env.SNAPSHOT_KEY;
+  return activityHandler(req, res);
+}
